69ad25c4a367b65558edb0bba7bab4fed4df4328
Alexandre Chaussier <alexandre.chaussier@temelio.com>
2018-01-04 07:56:54 +0100

    Update testing to work with Molecule 2.6.0

---
+++ molecule/default/tests/test_installation.py
@@ -0,0 +1,27 @@
+"""
+Role tests
+"""
+
+import os
+import pytest
+
+from testinfra.utils.ansible_runner import AnsibleRunner
+
+testinfra_hosts = AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize('name,distribution', [
+    ('locales', 'debian'),
+    ('locales', 'ubuntu'),
+    ('language-pack-en', 'ubuntu'),
+])
+def test_packages(host, name, distribution):
+    """
+    Check if packages are installed
+    """
+
+    if distribution != host.system_info.distribution:
+        pytest.skip('Test defined for another distribution')
+
+    assert host.package(name).is_installed
